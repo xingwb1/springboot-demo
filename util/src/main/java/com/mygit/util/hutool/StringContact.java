1c0ca4737e00e785ed90b9b2373f93b62d69331e
xingwubiaolol <xingwubiao@outlook.com>
2021-02-21 10:15:57 +0800

    hutool:StrUtil 拼接字符串

---
+++ util/src/main/java/com/mygit/util/hutool/StringContact.java
@@ -0,0 +1,18 @@
+package com.mygit.util.hutool;
+
+import cn.hutool.core.util.StrUtil;
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * Description:
+ *
+ * @author: 邢武彪
+ * Date: 2021-02-21
+ * Time: 10:08
+ */
+@Slf4j
+public class StringContact {
+    public static void main(String[] args) {
+        log.info(StrUtil.format("参数: {}", "参数"));
+    }
+}
